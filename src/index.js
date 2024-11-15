2e8ad83ce63c528af85584c9c93d3cbcf477e61b
DSwan <dianaswan0@gmail.com>
2024-11-15 12:07:15 +0000

    Added a div for fake content; JS query selector and event listener, called the function; css added for the fake content

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 document.querySelector("form").addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent the form from submitting
+  event.preventDefault();
 
-  // Fake content to display
   const fakePoem = `
       Your wings are strong, <br>
       Flap them into a song, <br>
       Soar high like an eagle.
     `;
 
-  // Add the fake content to the page
   document.querySelector("#fakeContent").innerHTML = fakePoem;
 });
